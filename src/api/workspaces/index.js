89479d66e1ba3059eb0a0c18886ded4244651442
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-03-17 17:54:12 +0530

    Added getWorkspaceById api

--- src/api/workspaces/index.js
+++ src/api/workspaces/index.js
@@ -31,3 +31,18 @@ export const fetchworkspaceRequest = async ({ token }) => {
         throw error.response.data;
     }
 };
+
+
+export const fetchWorkspaceDetailsRequest = async ({ workspaceId, token }) => {
+    try {
+        const response = await axios.get(`/workspaces/${workspaceId}`, {
+            headers: {
+                'x-access-token': token
+            }
+        });
+        return response.data?.data;
+    } catch (error) {
+        console.log('Error in fetching workspace details request', error);
+        throw error.response.data;
+    }
+};
---
+++ src/hooks/apis/workspaces/useGetWorkspaceById.js
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { fetchWorkspaceDetailsRequest } from '@/api/workspaces';
+import { useAuth } from '@/hooks/context/useAuth';
+
+export const useGetWorkspaceById = (id) => {
+    const { auth } = useAuth();
+    const { isFetching, isSuccess, error, data: workspace } = useQuery({
+        queryFn: (id) => fetchWorkspaceDetailsRequest({ workspaceId: id, token: auth?.token}),
+        queryKey: [`fetchWorkspaceById-${id}`],
+        staleTime: 10000
+    });
+
+    return {
+        isFetching,
+        isSuccess,
+        error,
+        workspace
+    };
+};
