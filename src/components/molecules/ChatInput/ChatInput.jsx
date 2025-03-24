ecbf7c2f431b949dc2c42567c7f12bd488cc0e8f
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-03-24 21:59:10 +0530

    Added quill get content

--- src/components/molecules/ChatInput/ChatInput.jsx
+++ src/components/molecules/ChatInput/ChatInput.jsx
@@ -1,13 +1,17 @@
 import { Editor } from '@/components/atoms/Editor/Editor';
 
 export const ChatInput = () => {
+
+    async function handleSubmit({ body }) {
+        console.log(body);
+    }
     return (
         <div
          className="px-5 w-full"
         >
             <Editor 
                placeholder="Type a message..."
-               onSubmit={() => {}}
+               onSubmit={handleSubmit}
                onCancel={() => {}}
                disabled={false}
                defaultValue=""
