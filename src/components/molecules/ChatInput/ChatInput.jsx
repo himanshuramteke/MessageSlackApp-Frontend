52f2917338006b0dd3e1394cec8ea5bb4a9e4d66
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-03-24 23:01:09 +0530

    Added new message sending event

--- src/components/molecules/ChatInput/ChatInput.jsx
+++ src/components/molecules/ChatInput/ChatInput.jsx
@@ -1,9 +1,24 @@
 import { Editor } from '@/components/atoms/Editor/Editor';
+import { useAuth } from '@/hooks/context/useAuth';
+import { useCurrentWorkspace } from '@/hooks/context/useCurrentWorkspace';
+import { useSocket } from '@/hooks/context/useSocket';
 
 export const ChatInput = () => {
 
+    const { socket, currentChannel } = useSocket();
+    const { auth } = useAuth();
+    const { currentWorkspace } = useCurrentWorkspace();
+
     async function handleSubmit({ body }) {
         console.log(body);
+        socket?.emit('NewMessage', {
+            channelId: currentChannel,
+            body,
+            senderId: auth?.user?.id,
+            workspaceId: currentWorkspace?._id
+        }, (data) => {
+            console.log('Message sent', data);
+        });
     }
     return (
         <div
