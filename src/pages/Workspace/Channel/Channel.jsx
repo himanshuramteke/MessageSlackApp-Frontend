478a52e95220d1f1ed3a89da77ec9a12ed9f032d
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-03-26 20:02:12 +0530

    Added scroll for message list

--- src/pages/Workspace/Channel/Channel.jsx
+++ src/pages/Workspace/Channel/Channel.jsx
@@ -1,6 +1,6 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { Loader2Icon, TriangleAlertIcon } from 'lucide-react';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { ChannelHeader } from '@/components/molecules/Channel/ChannelHeader';
@@ -22,6 +22,14 @@ export const Channel = () => {
     const { joinChannel } = useSocket();
 
     const { messages, isSuccess } = useGetChannelMessages(channelId);
+
+    const messageContainerListRef = useRef(null);
+
+    useEffect(() => {
+       if(messageContainerListRef.current) {
+        messageContainerListRef.current.scrollTop = messageContainerListRef.current.scrollHeight;
+       }
+    }, [messageList]);
 
     useEffect(() => {
        console.log('ChannelId', channelId);
@@ -65,9 +73,14 @@ export const Channel = () => {
         <div className='flex flex-col h-full'>
             <ChannelHeader name={channelDetails?.name} />
 
-            {messageList?.map((message) => {
+            <div
+               ref={messageContainerListRef}
+               className='flex-5 overflow-y-auto p-5 gap-y-2'
+            >
+                {messageList?.map((message) => {
                 return <Message key={message._id} body={message.body} authorImage={message.senderId?.avatar} authorName={message.senderId?.username} createdAt={message.createdAt} />;
             })}
+            </div>
             <div className='flex-1' />
             <ChatInput />
         </div>
